8516727b85b5a5bbb0ca74a37ab514b64fcc2ea0
Chris Kite <chris.kat82@googlemail.com>
2020-11-23 21:01:50 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>ToDo's</h1>
+    <form v-on:submit.prevent="saveNewTodo">
+      <label for="new-todo"></label>
+      <input id="new-todo" type="text" placeholder="Add new todo to list" v-model="newTodo"/>
+      <input type="radio" id="high" name="priority" value="priority">
+      <label for="high">High</label>
+      <input type="radio" id="low" name="priority" value="priority">
+      <label for="low">Low</label>
+      <input type="submit" value="Save new todo">
+    </form>
+
+    <ul>
+      <li v-for="(todo, index) in todos" v-bind:key="index">{{todo.task}}</li>
+    </ul>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      todos: [{task: "Walk the dog"},
+              {task: "Wash the dishes"}, 
+              {task: "Water the plants"}
+              ],
+              // couldn't get it to work without it being an object - todos: ["Walk the dog", "Wash the dishes", "Water the plants"],
+      newTodo: ""
+    }
+  },
+  methods: {
+    saveNewTodo: function(){
+      this.todos.push({
+        task: this.newTodo,
+        })
+      this.newTodo = "";
+    }
   }
 }
 </script>
